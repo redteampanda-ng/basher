e9e82089d9c34b35204239de2851bd70cc4f8c0e
redteampanda-ng <31235211+redteampanda-ng@users.noreply.github.com>
2021-06-19 21:52:12 +0200

    better argument handling

--- basher.sh
+++ basher.sh
@@ -1,53 +1,98 @@
 #!/bin/bash
 # Simple Port Scanner. I wrote that little script for environments where no netcat or other tools are available
 
-function scanMe {
-	printf "\033[0;33mThis script will check for open ports on one ore multiple remote hosts.\033[0m\n"
-	printf "All Ports and Hosts have to be separated by a \033[0;33mSPACE\033[0m e.g. 80 443 25 110 ... \n"
-	printf "\n"
-	printf "Which Hosts do you want to scan? "
-	read -a hosts
-	printf "What Ports do you want to scan? "
-	read -a ports
-	printf "\n"
+VERSION="v0.1"
 
-	if [[ $hosts == "" || $ports == "" ]]
-	then
-		printf "\033[0;33mHost or Port can't be empty!\033[0m\n"
-		scanMe
-	else
-		for host in ${hosts[@]}
-		do
-			for port in ${ports[@]}
-			do
-   				portScan $host $port 2>/dev/null
-			done
-		done
-		scanAgain
-	fi
-}
+# Colors
+C=$(printf '\033')
+RED="${C}[1;31m"
+SED_RED="${C}[1;31m&${C}[0m"
+GREEN="${C}[1;32m"
+SED_GREEN="${C}[1;32m&${C}[0m"
+YELLOW="${C}[1;33m"
+SED_YELLOW="${C}[1;33m&${C}[0m"
+SED_RED_YELLOW="${C}[1;31;103m&${C}[0m"
+BLUE="${C}[1;34m"
+SED_BLUE="${C}[1;34m&${C}[0m"
+ITALIC_BLUE="${C}[1;34m${C}[3m"
+LG="${C}[1;37m" #LightGray
+SED_LG="${C}[1;37m&${C}[0m"
+DG="${C}[1;90m" #DarkGray
+SED_DG="${C}[1;90m&${C}[0m"
+NC="${C}[0m"
+UNDERLINED="${C}[5m"
+ITALIC="${C}[3m"
 
-function portScan {
-	host=$1
-	port=$2
-		# change timeout according to your environment/connection quality. Higher timeout takes longer but might be more precise!
-		timeout 0.5 bash -c "echo >/dev/tcp/$host/$port" &&
-		printf "$host:$port -> \033[0;32m[open]\033[0m\n" ||
-		printf "$host:$port -> \033[0;31m[closed]\033[0m\n"
-}
+# Help
+HELP=$GREEN"Scan Hosts for open Ports.
+${NC}This script will scan hosts for open ports.
+    ${YELLOW}-h${BLUE} To show this message.
+    ${YELLOW}-s${BLUE} Scan a single host for open ports.
+    ${YELLOW}-m${BLUE} Scan multiple hosts for open ports.
+    ${YELLOW}-x${BLUE} Hosts to scan.
+    ${YELLOW}-p${BLUE} Ports to scan.
+    ${YELLOW}-i${BLUE} Use ping before scanning (make sure host responds to ICMP packets)
+    ${YELLOW}-w${BLUE} Wait time before the script will mark a port as closed (default 0.5)"
 
-# returning to main menu if needed
-function scanAgain {
-	printf "\033[0;33m\nStart another scan? [Y/n]: \033[0m"
-	read newScan
-	if [[ $newScan == "y" || $newScan == "Y" ]]
-	then
-		scanMe
-	else
-		printf "Quitting\n"
-		exit
-	fi
-}
+WAITPORT=0.5
+WAITICMP=3
 
-scanMe
+OPTION='h?smx:p:iw:'
 
+while getopts $OPTION flag; do
+    case "${flag}" in
+        h  ) printf "%s\n\n" "$HELP$NC" >&2; exit 1;;
+        s  ) SINGLE="true";;
+        m  ) MULTIPLE="true";;
+        x  ) HOSTS="${OPTARG}";;
+        p  ) PORTS="${OPTARG}";;
+        i  ) ICMP="true";;
+        w  ) WAITPORT="${OPTARG}";; # needs change to input level 1- 0.5s 2- 1s 3- 2s
+        \? ) printf "${RED}Unknown option: -%s\n" "$OPTARG$NC" >&2; printf "%s\n\n" "$HELP$NC" >&2; exit 1;;
+        :  ) printf "${YELLOW}Missing option argument for -%s\n" "$OPTARG$NC" >&2; printf "%s\n\n" "$HELP$NC" >&2; exit 1;;
+        *  ) printf "${YELLOW}Unimplemented option: -%2\n" "$OPTARG$NC" >&2; printf "%s\n\n" "$HELP$NC" >&2; exit 1;;
+        ### needs some tweaking, doesnt work that good!
+    esac
+done
+
+# mandatory arguments
+if [ ! "$PORTS" ] || [ ! $HOSTS ]; then
+    printf "${RED}Both Host(s) and Port(s) must be provided!${NC}\n\n"
+    printf "%s\n\n" "$HELP$NC" >&2; exit 1
+fi
+
+
+    # old stuff, needs to be reused later on maybe
+	#if [[ $hosts == "" || $ports == "" ]]
+	#then
+	#	printf "\033[0;33mHost or Port can't be empty!\033[0m\n"
+	#	scanMe
+	#else
+	#	for host in ${hosts[@]}
+	#	do
+	#		for port in ${ports[@]}
+	#		do
+   	#			portScan $host $port 2>/dev/null
+	#		done
+	#	done
+	#	scanAgain
+	#fi
+
+if [ $ICMP ]; then
+    timeout $WAITICMP bash -c "ping -c 1 ${HOSTS}" &> /dev/null &&
+    ONLINE=1 ||
+    ONLINE=0
+
+    if [ $ONLINE == 1 ]; then
+        printf "${GREEN}Host is online!${NC}\n"
+    else
+        printf "${YELLOW}Host seems offline. If you are sure that the host is online, omit -i option when running the script!${NC}\n"
+        printf "${YELLOW}Skipping host ${HOSTS}${NC}\n"
+        exit 1
+    fi
+fi
+
+timeout $WAITPORT bash -c "echo >/dev/tcp/$HOSTS/$PORTS" &> /dev/null &&
+printf "${BLUE}$HOSTS${NC}:${GREEN}$PORTS${NC} -> ${GREEN}[open]${NC}\n" ||
+printf "${BLUE}$HOSTS${NC}:${RED}$PORTS${NC} -> ${RED}[closed]${NC}\n"
+printf "done\n\n"
